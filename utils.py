f2f8dc7abc7d92e160298c5b41796c054fbe9bbf
alexandre abela <alexandre.abela@centrale-marseille.fr>
2024-02-28 17:47:43 +0100

    Stratified fine tuning

--- utils.py
+++ utils.py
@@ -23,8 +23,15 @@ def prompt_instruction_format(sample):
 def import_data_from_json(datapath): 
     data_df = pd.read_json(datapath).T.reset_index()
 
-    train_data, test_data = train_test_split(data_df, test_size=0.05)
-    train_data, val_data = train_test_split(data_df, test_size=0.15)
+    data_df['text_length'] = data_df['original_text'].apply(len)
+
+    max_length = data_df['text_length'].max()
+    bins = [0, max_length*0.2, max_length*0.4, max_length*0.6, max_length*0.8, max_length]
+    labels = ['1', '2', '3', '4', '5']
+    data_df['length_category'] = pd.cut(data_df['text_length'], bins=bins, labels=labels)
+
+    train_data, test_data = train_test_split(data_df, test_size=0.01, stratify=data_df['length_category'])
+    train_data, val_data = train_test_split(data_df, test_size=0.18, stratify=data_df['length_category'])
 
     columns_to_remove = [key for key in train_data if key not in ("original_text", "reference_summary")]
     
