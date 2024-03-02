7de1c14b7c4b75de248becb352580616d0e85b8a
alexandre abela <alexandre.abela@centrale-marseille.fr>
2024-03-02 18:17:07 +0100

    Finalizing README file

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Text_Summarization
+# Project title
+
+Our **Legal Text Summarizer** project, developed during the **Airbus Hackathon**, is a practical application of existing advancements in text summarization to address the challenges posed by extensive legal documents.
+
+The model is a combination of classical transformers for text summarization and RAG (Retrieval Augmented Generation). The classical transformers are the main components of the model as the RAG is quite energy hungry and we wanted to make an efficient model regarding energy consumption. 
+
+## Overview
+- [How to use](#howtouse)
+- [Examples](#examples)
+- [Model](#model)
+- [Results](#results)
+- [Authors](#auteurs)
+- [Licence](#licence)
+
+## How to use
+
+In order to use the text summarizer you must provide a config file in the yaml format :
+
+```yaml
+parameters_textsum_archi:
+  n_brut_force: 5
+
+models:
+  number: 2
+  models_names: ["google-t5/t5-base", "facebook/bart-base"]
+  models_params: [
+    ["./weight/google-t5/tokenizer", "./weight/google-t5/t5-base"],
+    ["./weight/facebook/tokenizer", "./weight/facebook/bart-base"]
+  ]
+
+modelsRAG: 
+  chunk_size: 1000
+  chunk_overlap: 100
+  api_key: "11111111111111111111111111111111111111"
+  repo_id: "mistralai/Mistral-7B-Instruct-v0.2"
+
+
+dbRAG: 
+  datapath: "./data.json"
+```
+
+There are two ways to use the text summarizer: 
+ - if you want to use the model to evaluate it on the dataset you should have a dataset to test it on : 
+```python
+python main.py --config_file_path config.yaml --data_test_path data.json --evaluation_mode
+```
+ - if you want to use the model just to summarize one text, you can use the following command and put the text in the input window 
+```python
+python main.py --config_file_path config.yaml 
+```
+
+## Examples
+
+1. Evaluation 
+
+2. One shot summary 
+
+## Model
+
+The model is composed of two part: 
+ - in the first part we use **classical transformers** trained on text summarization task like **T5 and BART** in order to produce summaries using **bootstrapping strategies** in order to choose the summary from the model that has the highest similarity with the original text. Then, we use an **ensemble strategy** to choose the summary with again the highest similarity between the outputs of T5 and BART
+ - the second part of the model is used only if the summary produced by T5 or BART does not have a **similarity higher than 0.85**. It's a **RAG** where the vector database is a database composed of the data provided in the config file. The **vector database is built using FAISS**. Then the prompt is enriched and passed through the LLM that has been chosen in the config file.
+
+![Model architecture](model_architecture.png)
+
+## Authors 
+
+- [Alexandre Abela](https://github.com/alexandreabela)
+- [Vincent Lébé](https://github.com/vlebe)
+- [Théo Saulais](https://github.com/tsaulais)
+- [Joey Skaf](https://github.com/jskaf34)
+
+## Licence
+
+This project is under the licence [MIT](https://opensource.org/license/mit).
--- main.py
+++ main.py
@@ -4,36 +4,53 @@ import pandas as pd
 
 from evaluate import evaluate
 from utils import import_data_from_json
-from text_summarizer_no_RAG import TextSummarizer
+from text_summarizer_RAG import TextSummarizerRAG
 
 def main(args):
-    test_dataset = import_data_from_json(args.data_test_path)
+    if args.config_file_path is None: 
+        raise ValueError("Config file for model is missing. Please provide a valid configuration.")
+    
+    text_summarizer = TextSummarizerRAG(config_file_path=args.config_file_path)
 
-    text_summarizer = TextSummarizer(config_file_path=args.config_file_path)
+    if args.evaluation_mode: 
+        if args.data_test_path is None: 
+            raise ValueError("Evaluation data is missing. Please provide a valid dataset.")
+        
+        test_dataset = import_data_from_json(args.data_test_path)
+        score_rouge, score_sim, perf_dict = evaluate(test_dataset, text_summarizer)
 
-    score_rouge, score_sim, perf_dict = evaluate(test_dataset, text_summarizer)
+        if args.output_folder is not None: 
+            pd.DataFrame(perf_dict['Rouge']).to_csv(os.path.join(args.output_folder, "rouge_score.csv"))
+            pd.DataFrame(perf_dict['Similarity']).to_csv(os.path.join(args.output_folder, "sim_score.csv"))
 
-    if args.output_folder is not None: 
-        pd.DataFrame(perf_dict['Rouge']).to_csv(os.path.join(args.output_folder, "rouge_score.csv"))
-        pd.DataFrame(perf_dict['Similarity']).to_csv(os.path.join(args.output_folder, "sim_score.csv"))
+            score = f"Score rouge is {score_rouge}. \n Score sim is {score_sim}."
+            text_file = open(os.path.join(args.output_folder, "score.txt"))
+            text_file.write(score)
+            text_file.close()
 
-        score = f"Score rouge is {score_rouge}. \n Score sim is {score_sim}."
-        text_file = open(os.path.join(args.output_folder, "score.txt"))
-        text_file.write(score)
-        text_file.close()
+        else: 
+            print(pd.DataFrame(perf_dict['Rouge']))
+            print("\n")
 
-    else: 
-        print(pd.DataFrame(perf_dict['Rouge']))
-        print("\n")
+            print(pd.DataFrame(perf_dict['Similarity']))
+            print("\n")
 
-        print(pd.DataFrame(perf_dict['Similarity']))
-        print("\n")
+            print(f"Score rouge is {score_rouge}")
+            print("\n")
 
-        print(f"Score rouge is {score_rouge}")
-        print("\n")
+            print(f"Score sim is {score_sim}")
+            print("\n")
 
-        print(f"Score sim is {score_sim}")
-        print("\n")
+    else:
+        input_text = input("What do you want to summarize ? \n")
+
+        summary = text_summarizer(input_text)
+
+        dash_line = '-'.join('' for _ in range(100))
+        print(dash_line)
+        print(f'INPUT PROMPT:\n{input_text}')
+        print(dash_line)
+        print(f'MODEL SUMMARY:\n{summary}\n')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -55,4 +72,11 @@ if __name__ == "__main__":
         type=str, 
         help="The folder in which to put the result"
     )
+    parser.add_argument(
+        "--evaluation_mode", 
+        action="store_true",
+        type=bool, 
+        help="Whether or not to use the model for evaluation according to the hackathon template. "
+    )
+
     main(parser.parse_args())
